0d2a63f818510bda07efffa3f510644a45574478
ptrwilk <ptrwilk@outlook.com>
2023-01-15 08:21:50 +0100

    LL-17 - fix - menu white on apk version

--- App.tsx
+++ App.tsx
@@ -1,21 +1,23 @@
-import { StyleSheet, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text } from "react-native";
 import Heading from "./src/Core/Heading";
 import Main from "./src/Core/Main";
 import { Colors } from "./src/styles";
 
 const Header = () => {
-  return (
-    <View style={{ height: 30, backgroundColor: Colors.WhiteColor }}></View>
-  );
+  return <View style={{ height: 30, backgroundColor: Colors.WhiteColor }} />;
 };
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Header />
-      <Heading />
-      <Main />
-    </View>
+    <>
+      <StatusBar backgroundColor={Colors.WhiteColor} />
+      <View style={styles.container}>
+        <Header />
+        <Heading />
+        <Main />
+      </View>
+    </>
   );
 }
 
